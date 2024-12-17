9937acba27c75c99b5f192dd68337b4f326522c8
Paul Laffitte <plaffitt@gmail.com>
2024-12-17 22:05:38 +0100

    add show command

--- histctx.sh
+++ histctx.sh
@@ -16,6 +16,34 @@ function get_current_context {
 	fi
 }
 
+function list_contexts_names {
+	contexts=$(find "$HOME/.bash_history.d" -type f)
+	for context in $contexts; do
+		basename "$context"
+	done
+}
+
+function cmd_show {
+	if [ $# -eq 0 ]; then
+		set -- $(list_contexts_names)
+	fi
+	max_length=0
+	for context in "$@"; do
+		if [[ ! -f "$HOME/.bash_history.d/$context" ]]; then
+			echo "invalid argument: \"$context\" doesn't exist"
+			exit 1
+		fi
+		local length=${#context}
+		if (( length > max_length )); then
+			max_length=$length
+		fi
+	done
+	for context in "$@"; do
+		name_colunm=$(printf "%-${max_length}s" "$context")
+		awk "{print \"$name_colunm | \" \$0}" "$HOME/.bash_history.d/$context"
+	done
+}
+
 function cmd_list {
 	echo -e "LINES\tLAST ACCESS      \tNAME"
 	contexts=$(find "$HOME/.bash_history.d" -type f)
@@ -50,14 +78,17 @@ COMMANDS
   set                      set a context, create it if it doesn't exists yet. \"$0 set .\" expands \".\" as the current folder name
   temporary|tmp            create a context in /tmp and set it as current
   list|ls                  list existing history contexts
+  show|cat                 show history of given contexts, or all histories if no argument is given
   rename|mv                rename an history context
   delete|del|remove|rm     delete an history context
   help                     show this help message
 
 EXAMPLES
   $ histctx set new-context
+  $ histctx set .
   $ histctx tmp
   $ histctx list
+  $ histctx show my-context another-context
   $ histctx rename new-context my-context
   $ histctx delete my-context"
 }
@@ -104,6 +135,9 @@ temporary | tmp)
 	;;
 list | ls)
 	cmd_list
+	;;
+show | cat)
+	cmd_show "${@:2}"
 	;;
 rename | mv)
 	cmd_rename "${@:2}"
